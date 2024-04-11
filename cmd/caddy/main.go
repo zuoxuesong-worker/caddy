deae5913cc903399562d53ce31c3f83a5d0146be
liuzhen <liuzhen@yunify.com>
2024-04-11 16:01:59 +0800

    add jwt & layer4

--- cmd/caddy/main.go
+++ cmd/caddy/main.go
@@ -33,6 +33,8 @@ import (
 
 	// plug in Caddy modules here
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
+	_ "github.com/liuzhen21/caddy-jwt"
+	_ "github.com/mholt/caddy-l4"
 )
 
 func main() {
